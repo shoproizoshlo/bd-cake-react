4e78996fbc3fd27fd031f6dc561fd2d477c3d679
shoproizoshlo <iamsusunny@gmail.com>
2023-12-14 12:49:53 +0100

    how to show candles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [age, setAge] = useState([]);
+
+  const generateElements = () => {
+    const candles = [];
+    for (let i = 0; i < age; i++) {
+      candles.push(
+        <div className="bd-candle" key={i}>
+          <div className="candle"></div>
+          <div className="fire"></div>
+        </div>
+      );
+    }
+    return candles;
+  };
+
+  // bd-candle:
+  // if age >=1
+  // display: flex;
+  // for each 1 year display 1 bd-candle
+  // else
+  // display none
+
+  // if blow
+  // fire display: none;
+  // <p>candles left</p>
   return (
     <>
+      <div className="age">
+        <input
+          type="number"
+          placeholder="Enter your age"
+          value={age}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setAge(e.target.value);
+          }}
+        />
+      </div>
       <div className="bd-cake">
         <div className="cake circle-1"></div>
         <div className="cake circle-2"></div>
         <div className="cake circle-3"></div>
       </div>
-      <div className="bd-candle">
-        <div className="candle"></div>
-        <div className="fire"></div>
-      </div>
+      {generateElements()}
     </>
   );
 }
