827a79bb9d5f96c855942d70ba6fedb24a251e71
shoproizoshlo <iamsusunny@gmail.com>
2024-01-02 16:42:48 +0100

    synchronised candles and ages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import "./App.scss";
 
 function App() {
+  const [age, setAge] = useState([]);
+  const [elementPositions, setElementPositions] = useState([]);
+
+  useEffect(() => {
+    const newPositions = Array.from(
+      { length: age - elementPositions.length },
+      () => ({
+        x: Math.random() * 230 + 10,
+        y: Math.random() * 20,
+      })
+    );
+
+    setElementPositions((prevPositions) => [...prevPositions, ...newPositions]);
+  }, [age, elementPositions.length]);
+
   return (
     <>
-      <input type="number" />
+      <input
+        type="number"
+        value={age}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setAge(e.target.value);
+        }}
+      />
+
       <div className="cake">
         <div className="plate"></div>
         <div className="layer layer-bottom"></div>
@@ -14,10 +38,22 @@ function App() {
         <div className="drip drip1"></div>
         <div className="drip drip2"></div>
         <div className="drip drip3"></div>
-        <div className="candle">
-          <div className="flame"></div>
+        <div className="bd-candles">
+          {elementPositions.map((position, i) => (
+            <div
+              className="candle"
+              style={{
+                position: "absolute",
+                left: `${position.x}px`,
+                top: `${position.y}px`,
+              }}
+              key={i}
+            >
+              <div className="flame"></div>
+              <div className="drop"></div>
+            </div>
+          ))}
         </div>
-        <div className="drop"></div>
       </div>
     </>
   );
